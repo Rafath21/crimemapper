78fc5076472323e52d68910e8f7a4568c927f046
Rafath21 <rafathmirza05@gmail.com>
2021-04-21 02:24:47 +0530

    commiting

---
+++ districts.js
@@ -0,0 +1,452 @@
+const districtList = [
+    {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.4867,17.3850]
+           },
+           "properties": {
+             "name": "hyderabad",
+             "murder": "78",
+             "kidnapping":"70",
+             "rapes":"167",
+             "hurtCases":"1369",
+             "Riotings":"37",
+             "Thefts":"2953",
+           }
+         },
+          {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.1288, 18.4386]
+           },
+           "properties": {
+             "name": "karimnagar",
+             "murder": "13",
+             "kidnapping":"29",
+             "rapes":"37",
+             "hurtCases":"622",
+             "Riotings":"37",
+             "Thefts":"263",
+           }
+         },
+   
+      {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [80.1514, 17.2473]
+           },
+           "properties": {
+             "name": "khammam",
+             "murder": "24",
+             "kidnapping":"44",
+             "rapes":"58",
+             "hurtCases":"714",
+             "Riotings":"37",
+             "Thefts ":"334",
+           }
+         } ,
+            {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.0941, 18.6725]
+           },
+           "properties": {
+             "name": "nizamabad",
+             "murder": "39",
+             "kidnapping":"14",
+             "rapes":"24",
+             "hurtCases":"496",
+             "Riotings":"4",
+             "Thefts ":"449",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.8031, 17.1813]
+           },
+           "properties": {
+             "name": "Rachakonda",
+             "murder": "88",
+             "kidnapping":"142",
+             "rapes":"164",
+             "hurtCases":"1230",
+             "Riotings":"25",
+             "Thefts ":"1692",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.5134, 18.7519]
+           },
+           "properties": {
+             "name": "Ramagundam",
+             "murder": "38",
+             "kidnapping":"32",
+             "rapes":"33",
+             "hurtCases":"828",
+             "Riotings":"10",
+             "Thefts ":"376",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.8520, 18.1018]
+           },
+           "properties": {
+             "name": "Siddipet",
+             "murder": "17",
+             "kidnapping":"13",
+             "rapes":"30",
+             "hurtCases":"505",
+             "Riotings":"29",
+             "Thefts ":"185",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.5941, 17.9689]
+           },
+           "properties": {
+             "name": "Warangal",
+             "murder": "52",
+             "kidnapping":"46",
+             "rapes":"58",
+             "hurtCases":"1256",
+             "Riotings":"65",
+             "Thefts ":"330",
+           }
+         },
+          {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.5320, 19.6641]
+           },
+           "properties": {
+             "name": "Adilabad",
+             "murder": "16",
+             "kidnapping":"12",
+             "rapes":"27",
+             "hurtCases":"340",
+             "Riotings":"2",
+             "Thefts ":"162",
+           }
+         },
+          {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [80.619736, 17.554356]
+           },
+           "properties": {
+             "name": "Bhadradri Kothagudem",
+             "murder": "25",
+             "kidnapping":"44",
+             "rapes":"77",
+             "hurtCases":"553",
+             "Riotings":"49",
+             "Thefts ":"210",
+           }
+         },
+          {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [78.9120, 18.7895]
+           },
+           "properties": {
+             "name": "Jagityal",
+             "murder": "30",
+             "kidnapping":"13",
+             "rapes":"32",
+             "hurtCases":"351",
+             "Riotings":"12",
+             "Thefts ":"113",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.8635, 18.4293]
+           },
+           "properties": {
+             "name": "Jayashankar Bhupalpalli",
+             "murder": "22",
+             "kidnapping":"32",
+             "rapes":"28",
+             "hurtCases":"409",
+             "Riotings":"6",
+             "Thefts ":"122",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [77.79556, 16.23504]
+           },
+           "properties": {
+             "name": "Jogulamba Gadwal",
+             "murder": "20",
+             "kidnapping":"9",
+             "rapes":"23",
+             "hurtCases":"218",
+             "Riotings":"8",
+             "Thefts ":"194",
+           }
+         },
+          {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.8635, 18.4293]
+           },
+           "properties": {
+             "name": "Gadwal",
+             "murder": "20",
+             "kidnapping":"9",
+             "rapes":"23",
+             "hurtCases":"218",
+             "Riotings":"8",
+             "Thefts ":"194",
+           }
+         },
+         {
+           "type": "Feature",
+           "geometry": {
+             "type": "Point",
+             "coordinates": [79.8635, 18.4293]
+           },
+           "properties": {
+             "name": "Kamareddy",
+             "murder": "22",
+             "kidnapping":"15",
+             "rapes":"12",
+             "hurtCases":"325",
+             "Riotings":"5",
+             "Thefts ":"211",
+           }
+         },
+         {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [79.274, 19.365]
+          },
+          "properties": {
+            "name": "Kumarambheem Asifabad",
+            "murder": "15",
+            "kidnapping":"2",
+            "rapes":"22",
+            "hurtCases":"245",
+            "Riotings":"2",
+            "Thefts ":"41",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [80.0036, 17.6026]
+          },
+          "properties": {
+            "name": "Mahabubabad",
+            "murder": "19",
+            "kidnapping":"32",
+            "rapes":"32",
+            "hurtCases":"443",
+            "Riotings":"21",
+            "Thefts ":"85",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.1108, 16.3841]
+          },
+          "properties": {
+            "name": "Mahabubnagar",
+            "murder": "42",
+            "kidnapping":"30",
+            "rapes":"64",
+            "hurtCases":"456",
+            "Riotings":"14",
+            "Thefts ":"335",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.1108, 17.8716]
+          },
+          "properties": {
+            "name": "Medak",
+            "murder": "34",
+            "kidnapping":"4",
+            "rapes":"26",
+            "hurtCases":"285",
+            "Riotings":"5",
+            "Thefts ":"121",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.3102, 16.4939]
+          },
+          "properties": {
+            "name": "Nagarkurnool",
+            "murder": "24",
+            "kidnapping":"6",
+            "rapes":"23",
+            "hurtCases":"321",
+            "Riotings":"2",
+            "Thefts ":"266",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [79.2684,17.0575]
+          },
+          "properties": {
+            "name": " Nalgonda",
+            "murder": "40",
+            "kidnapping":"22",
+            "rapes":"41",
+            "hurtCases":"1011",
+            "Riotings":"19",
+            "Thefts ":"214",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.3430,19.0964]
+          },
+          "properties": {
+            "name": " Nirmal",
+            "murder": "12",
+            "kidnapping":"8",
+            "rapes":"15",
+            "hurtCases":"376",
+            "Riotings":"16",
+            "Thefts ":"112",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.8092,18.3889]
+          },
+          "properties": {
+            "name": " Rajanna Sircilla",
+            "murder": "16",
+            "kidnapping":"19",
+            "rapes":"12",
+            "hurtCases":"220",
+            "Riotings":"11",
+            "Thefts ":"67",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.0816,17.6140]
+          },
+          "properties": {
+            "name": "Sangareddy",
+            "murder": "60",
+            "kidnapping":"8",
+            "rapes":"36",
+            "hurtCases":"531",
+            "Riotings":"23",
+            "Thefts ":"304",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [79.6336,17.1314]
+          },
+          "properties": {
+            "name": "Suryapet",
+            "murder": "42",
+            "kidnapping":"14",
+            "rapes":"46",
+            "hurtCases":"741",
+            "Riotings":"16",
+            "Thefts ":"183",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [77.9048,17.3365]
+          },
+          "properties": {
+            "name": "Vikarabad",
+            "murder": "36",
+            "kidnapping":"18",
+            "rapes":"47",
+            "hurtCases":"344",
+            "Riotings":"3",
+            "Thefts ":"119",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.0622,16.3623]
+          },
+          "properties": {
+            "name": "Wanaparthy",
+            "murder": "17",
+            "kidnapping":"7",
+            "rapes":"20",
+            "hurtCases":"209",
+            "Riotings":"7",
+            "Thefts ":"135",
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.7936,17.7480]
+          },
+          "properties": {
+            "name": "RP Secunderabad",
+            "murder": "2",
+            "kidnapping":"1",
+            "rapes":"0",
+            "hurtCases":"9",
+            "Riotings":"1",
+            "Thefts ":"576",
+          }
+        },
+        
+   ]
